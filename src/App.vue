9c93648666a63cab33ea67df6b65e2a8158af3ba
Vidalic740 <charlesonvidalic740@gmail.com>
2025-05-08 18:09:33 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div className = "main">
+    <div className = "sidebar">
+      <nav>
+        <ul>
+          <li>Dashboard</li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main{
+  height: 95vh;
+  width: 98%;
+}
+
+.sidebar{
+  width: 15%;
+  height: 100vh;
+  background-color: #782673;
+}
+
+</style>
